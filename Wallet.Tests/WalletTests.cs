3d6a580c02876ce53ce05a2807a8333d4358a3ac
sebastian.przyszlak <sebastian.przyszlak@ex7.pl>
2024-01-05 23:12:21 +0100

    Delete unnecessary tests

--- Wallet.Tests/WalletTests.cs
+++ Wallet.Tests/WalletTests.cs
@@ -100,22 +100,4 @@ public class WalletTests {
     Money result = bank.Reduce(sum, "USD");
     Assert.Equal(Money.Dollar(25), result);
   }
-  
-  [Fact]
-  public void TestSumTimesDecimal() {
-    Expression fiveBucks = Money.Dollar(5);
-    Expression tenFrancs = Money.Franc(10);
-    Bank bank = new();
-    bank.AddRate("CHF", "USD", 2);
-    Expression sum = new Sum(fiveBucks, tenFrancs).Times(2m);
-    Money result = bank.Reduce(sum, "USD");
-    Assert.Equal(Money.Dollar(20), result);
-  }
-
-  [Fact]
-  public void TestExpressionTimesDecimal() {
-    Expression fiveBucks = Money.Dollar(5);
-    Expression result = fiveBucks.Times(2m);
-    Assert.Equal(Money.Dollar(10), result);
-  }
 }
