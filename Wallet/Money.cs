92f3f50479bf88cea7b78d6b6b70fc907696a683
frognar <sebastian.przyszlak@gmail.com>
2024-01-06 09:55:57 +0100

    Make Dollar and Franc factory methods with expression bodies

--- Wallet/Money.cs
+++ Wallet/Money.cs
@@ -1,13 +1,8 @@
 ﻿namespace Wallet;
 
 public readonly record struct Money(decimal Amount, string Currency) : Expression {
-  public static Money Dollar(decimal amount) {
-    return new Money(amount, "USD");
-  }
-
-  public static Money Franc(decimal amount) {
-    return new Money(amount, "CHF");
-  }
+  public static Money Dollar(decimal amount) => new(amount, "USD");
+  public static Money Franc(decimal amount) => new(amount, "CHF");
 
   public Expression Times(decimal multiplier) {
     return new Money(Amount * multiplier, Currency);
