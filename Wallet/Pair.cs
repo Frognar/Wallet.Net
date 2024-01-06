830bdd09d9b8ed421897d7c86cb93395f68d3f70
frognar <sebastian.przyszlak@gmail.com>
2024-01-06 09:35:50 +0100

    Make Pair a readonly record struct

--- Wallet/Pair.cs
+++ Wallet/Pair.cs
@@ -1,22 +1,3 @@
 ﻿namespace Wallet;
 
-public class Pair {
-  readonly string from;
-  readonly string to;
-
-  public Pair(string from, string to) {
-    this.from = from;
-    this.to = to;
-  }
-
-  public override bool Equals(object obj) {
-    if (obj is Pair pair) {
-      return from.Equals(pair.from) && to.Equals(pair.to);
-    }
-    return false;
-  }
-
-  public override int GetHashCode() {
-    return 0;
-  }
-}
+public readonly record struct Pair(string From, string To);
